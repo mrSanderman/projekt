08e22b4f37854146d91f43f6f71f66cce72bfbce
mrSanderman <mrSanderman@mail.ru>
2020-03-25 16:48:37 +0600

    my Commit

---
+++ script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+let a = 5;
+console.log(a)
+
+
+var number = 5;
+var string = "Hello";
+var syn = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0)
+console.log("string" * 5)
+
+let sim;
+console.log(sim);
+
+let persone = {
+    name: "john",
+    age: 25,
+    isMerried: false
+}
+
+// console.log(persone["name"])
+
+//  let array = ["plum.png", "orange.jpg", "apple.bmp"]
+
+// // console.log(array[0]);
+// // // alert("hello world");
+
+//  let answer = +prompt("Есть ли вам 18?", "Нет");
+
+// console.log(typeof(answer));
+// // console.log("arr " + " - objekt");
+// console.log(5 + +" - objekt");
+
+let  incr  = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+console.log(20%3)
+
+console.log("2" === 2)
+
+let isChecked = false,
+    isClose = false;
+console.log(isChecked || !isClose)
